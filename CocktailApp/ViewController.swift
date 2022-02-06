21bd42bb5fa93e29147cd5163790a57eb933f242
Leisan Latypova <latley90@gmail.com>
2022-02-06 10:53:40 +0300

    Initial Commit

---
+++ CocktailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailApp
+//
+//  Created by Leysan Latypova on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
